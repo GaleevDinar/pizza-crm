2af2b4605242e5019179530a283d232d7a780421
GaleevDinar <xxxcorsair@yandex.ru>
2018-05-19 16:17:00 +0300

    init

---
+++ gismart/src/main/java/ru/javamentor/gismart/GismartApplication.java
@@ -0,0 +1,12 @@
+package ru.javamentor.gismart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GismartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GismartApplication.class, args);
+    }
+}
